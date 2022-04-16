12bbe97dc20f1043bbd72b0a6a6a0cd8535e831b
mnkhod <mnkhod.dev@gmail.com>
2022-04-16 18:20:01 +0800

    Added addToMetamaskLink to Mumbai TokenList

--- constants.js
+++ constants.js
@@ -9,56 +9,104 @@ const mumbaiTokenList = {
       "name": "dTether USD",
       "symbol": "dUSDT",
       "decimals": 6,
-      "logoURI": "https://cryptologos.cc/logos/tether-usdt-logo.png?v=022"
+      "logoURI": "https://cryptologos.cc/logos/tether-usdt-logo.png?v=022",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=dTether%20USD` +
+        `&symbol=dUSDT` +
+        `&decimals=6` +
+        `&address=0xECd313e29b85cAf347fb832F80427602030cD3Fc` +
+        `&imgUrl=${"https://cryptologos.cc/logos/tether-usdt-logo.png?v=022"}`,
     },
     {
       "address": "0x356058b999d315Ef9F6d13f1C478591E9C667831",
       "name": "Diverse ARDX",
       "symbol": "dARDX",
       "decimals": 18,
-      "logoURI": "https://ardcoin.com/wp-content/themes/ardcoin/images/logo/logo3.png"
+      "logoURI": "https://ardcoin.com/wp-content/themes/ardcoin/images/logo/logo3.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Diverse%20ARDX` +
+        `&symbol=dARDX` +
+        `&decimals=18` +
+        `&address=0x356058b999d315Ef9F6d13f1C478591E9C667831` +
+        `&imgUrl=${"https://ardcoin.com/wp-content/themes/ardcoin/images/logo/logo3.png"}`,
     },
     {
       "address": "0xaB57fAf3b573B8ac1ad90255f6cF4E92DbbcCE91",
       "name": "Diverse DAI",
       "symbol": "dDAI",
       "decimals": 18,
-      "logoURI": "https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png?v=022"
+      "logoURI": "https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png?v=022",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Diverse%20DAI` +
+        `&symbol=dDAI` +
+        `&decimals=18` +
+        `&address=0xaB57fAf3b573B8ac1ad90255f6cF4E92DbbcCE91` +
+        `&imgUrl=${"https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png?v=022"}`,
     },
     {
       "address": "0x2b8920cBdDCc3e85753423eEceCd179cb9232554",
       "name": "Diverse USDC",
       "symbol": "dUSDC",
       "decimals": 6,
-      "logoURI": "https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=022"
+      "logoURI": "https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=022",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Diverse%20USDC` +
+        `&symbol=dUSDC` +
+        `&decimals=6` +
+        `&address=0x2b8920cBdDCc3e85753423eEceCd179cb9232554` +
+        `&imgUrl=${"https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=022"}`,
     },
     {
       "address": "0x8A250B3517AD8d59354D50af0D9be5c4Cd90F070",
       "name": "DummyToken-A",
       "symbol": "TKN-A",
       "decimals": 18,
-      "logoURI": "https://faucet.dsolutions.mn/_next/image?url=https%3A%2F%2Ffaucet.dsolutions.mn%2Fdiversetoken.png&w=640&q=75"
+      "logoURI": "https://faucet.dsolutions.mn/diversetoken.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=DummyToken-A` +
+        `&symbol=TKN-A` +
+        `&decimals=18` +
+        `&address=0x8A250B3517AD8d59354D50af0D9be5c4Cd90F070` +
+        `&imgUrl=${"https://faucet.dsolutions.mn/diversetoken.png"}`,
     },
     {
       "address": "0x551181Be541f56ce6C6c13448F54Adb8eA2AB531",
       "name": "DummyToken-B",
       "symbol": "TKN-B",
       "decimals": 18,
-      "logoURI": "https://faucet.dsolutions.mn/_next/image?url=https%3A%2F%2Ffaucet.dsolutions.mn%2Fdiversetoken.png&w=640&q=75"
+      "logoURI": "https://faucet.dsolutions.mn/diversetoken.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=DummyToken-B` +
+        `&symbol=TKN-B` +
+        `&decimals=18` +
+        `&address=0x551181Be541f56ce6C6c13448F54Adb8eA2AB531` +
+        `&imgUrl=${"https://faucet.dsolutions.mn/diversetoken.png"}`,
     },
     {
       "address": "0x14cD48F91D3ca4FE9C7e2e888d68667879942A9D",
       "name": "DummyToken-C",
       "symbol": "TKN-C",
       "decimals": 18,
-      "logoURI": "https://faucet.dsolutions.mn/_next/image?url=https%3A%2F%2Ffaucet.dsolutions.mn%2Fdiversetoken.png&w=640&q=75"
+      "logoURI": "https://faucet.dsolutions.mn/diversetoken.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=DummyToken-C` +
+        `&symbol=TKN-C` +
+        `&decimals=18` +
+        `&address=0x14cD48F91D3ca4FE9C7e2e888d68667879942A9D` +
+        `&imgUrl=${"https://faucet.dsolutions.mn/diversetoken.png"}`,
     },
     {
       "address": "0x76Cc59faf54D6262680049D4B5c74e7A587C8849",
       "name": "DummyToken-D",
       "symbol": "TKN-D",
       "decimals": 18,
-      "logoURI": "https://faucet.dsolutions.mn/_next/image?url=https%3A%2F%2Ffaucet.dsolutions.mn%2Fdiversetoken.png&w=640&q=75"
+      "logoURI": "https://faucet.dsolutions.mn/diversetoken.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=DummyToken-D` +
+        `&symbol=TKN-D` +
+        `&decimals=18` +
+        `&address=0x76Cc59faf54D6262680049D4B5c74e7A587C8849` +
+        `&imgUrl=${"https://faucet.dsolutions.mn/diversetoken.png"}`,
     },
   ]
 }
