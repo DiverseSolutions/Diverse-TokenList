409d19c90e13fc8b114fc7e6cdc750117f63816e
mnkhod <mnkhod.dev@gmail.com>
2022-04-26 21:17:46 +0800

    Added Mongolian Tokens To Mumbai List

--- constants.js
+++ constants.js
@@ -22,13 +22,13 @@ const mumbaiTokenList = {
       "name": "Diverse ARDX",
       "symbol": "dARDX",
       "decimals": 18,
-      "logoURI": "https://ardcoin.com/wp-content/themes/ardcoin/images/logo/logo3.png",
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/ardx.png",
       addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
         `name=Diverse%20ARDX` +
         `&symbol=dARDX` +
         `&decimals=18` +
         `&address=0x356058b999d315Ef9F6d13f1C478591E9C667831` +
-        `&imgUrl=${"https://ardcoin.com/wp-content/themes/ardcoin/images/logo/logo3.png"}`,
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/ardx.png"}`,
     },
     {
       "address": "0xaB57fAf3b573B8ac1ad90255f6cF4E92DbbcCE91",
@@ -95,18 +95,256 @@ const mumbaiTokenList = {
         `&address=0x14cD48F91D3ca4FE9C7e2e888d68667879942A9D` +
         `&imgUrl=${"https://faucet.dsolutions.mn/diversetoken.png"}`,
     },
+
+
+
+
     {
-      "address": "0x76Cc59faf54D6262680049D4B5c74e7A587C8849",
-      "name": "DummyToken-D",
-      "symbol": "TKN-D",
+      "address": "0xFC509bDc93c99BA9006115394E24f90dA54CD83b",
+      "name": "ArdMoney",
+      "symbol": "ARDM",
       "decimals": 18,
-      "logoURI": "https://faucet.dsolutions.mn/diversetoken.png",
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/ardm.png",
       addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
-        `name=DummyToken-D` +
-        `&symbol=TKN-D` +
+        `name=ArdMoney` +
+        `&symbol=ARDM` +
         `&decimals=18` +
-        `&address=0x76Cc59faf54D6262680049D4B5c74e7A587C8849` +
-        `&imgUrl=${"https://faucet.dsolutions.mn/diversetoken.png"}`,
+        `&address=0xFC509bDc93c99BA9006115394E24f90dA54CD83b` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/ardm.png"}`,
+    },
+    {
+      "address": "0x391326ad093d03679e70CD0F0C03425207a62A13",
+      "name": "BariCoin",
+      "symbol": "BARI",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/bari.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=BariCoin` +
+        `&symbol=BARI` +
+        `&decimals=18` +
+        `&address=0x391326ad093d03679e70CD0F0C03425207a62A13` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/bari.png"}`,
+    },
+    {
+      "address": "0xE78a5EB1A3C2Ff447Ee3AD30F8d6669DA6655557",
+      "name": "MGLcalceum",
+      "symbol": "CAEL",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/cael.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=MGLcalceum` +
+        `&symbol=CAEL` +
+        `&decimals=18` +
+        `&address=0xE78a5EB1A3C2Ff447Ee3AD30F8d6669DA6655557` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/cael.png"}`,
+    },
+    {
+      "address": "0x531c29f6719c0a0C8ECE08d69C75baf32fbA84C6",
+      "name": "CoinHub",
+      "symbol": "CHB",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/chb.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=CoinHub` +
+        `&symbol=CHB` +
+        `&decimals=18` +
+        `&address=0x531c29f6719c0a0C8ECE08d69C75baf32fbA84C6` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/chb.png"}`,
+    },
+    {
+      "address": "0xbE3FA6E8F34b3E0Ecbd36D4ca2B68dCf83511037",
+      "name": "CapexCoin",
+      "symbol": "CPX",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/cpx.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=CapexCoin` +
+        `&symbol=CPX` +
+        `&decimals=18` +
+        `&address=0xbE3FA6E8F34b3E0Ecbd36D4ca2B68dCf83511037` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/cpx.png"}`,
+    },
+    {
+      "address": "0x8782c6d553097ef1a2c7dD9F43533e472b0DE6d9",
+      "name": "Corex",
+      "symbol": "CRX",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/crx.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Corex` +
+        `&symbol=CRX` +
+        `&decimals=18` +
+        `&address=0x8782c6d553097ef1a2c7dD9F43533e472b0DE6d9` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/crx.png"}`,
+    },
+    {
+      "address": "0x4a8EA3B359aB916ffCe17Bf3d9ee18294A1d5944",
+      "name": "Digital Assets Coin",
+      "symbol": "DAC",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/dac.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Digital%20Assets%20Coin` +
+        `&symbol=DAC` +
+        `&decimals=18` +
+        `&address=0x4a8EA3B359aB916ffCe17Bf3d9ee18294A1d5944` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/dac.png"}`,
+    },
+    {
+      "address": "0x896af58cFE007EdF0bdF181aB272b00b86881B16",
+      "name": "DogeChew Coin",
+      "symbol": "DC",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/dc.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=DogeChew%20Coin` +
+        `&symbol=DC` +
+        `&decimals=18` +
+        `&address=0x896af58cFE007EdF0bdF181aB272b00b86881B16` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/dc.png"}`,
+    },
+    {
+      "address": "0xD5Ae54a6ed33A7c2583BFa7c7D140cB2E84aAd7e",
+      "name": "Elf Coin",
+      "symbol": "ELFC",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/elfc.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Elf%20Coin` +
+        `&symbol=ELFC` +
+        `&decimals=18` +
+        `&address=0xD5Ae54a6ed33A7c2583BFa7c7D140cB2E84aAd7e` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/elfc.png"}`,
+    },
+    {
+      "address": "0x1fD53F74FBdF2Ba9f535a3b281A47a28172BB658",
+      "name": "Ger Coin",
+      "symbol": "GER",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/ger.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Ger%20Coin` +
+        `&symbol=GER` +
+        `&decimals=18` +
+        `&address=0x1fD53F74FBdF2Ba9f535a3b281A47a28172BB658` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/ger.png"}`,
+    },
+    {
+      "address": "0xDd9A2276a2Fcb82Cf836E0FcF39b09A5717f9eCf",
+      "name": "Inflation Hedging Coin",
+      "symbol": "IHC",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/ihc.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Inflation%20Hedging%20Coin` +
+        `&symbol=IHC` +
+        `&decimals=18` +
+        `&address=0xDd9A2276a2Fcb82Cf836E0FcF39b09A5717f9eCf` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/ihc.png"}`,
+    },
+    {
+      "address": "0x83131194FdAB3E16633C95794eb36736a0562d90",
+      "name": "Mongol NFT",
+      "symbol": "MNFT",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/mnft.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Mongol%20NFT` +
+        `&symbol=MNFT` +
+        `&decimals=18` +
+        `&address=0x83131194FdAB3E16633C95794eb36736a0562d90` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/mnft.png"}`,
+    },
+    {
+      "address": "0x372beb3466036F011778726643AD609544360E8E",
+      "name": "Mongol Tether",
+      "symbol": "MONT",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/mont.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Mongol%20Tether` +
+        `&symbol=MONT` +
+        `&decimals=18` +
+        `&address=0x372beb3466036F011778726643AD609544360E8E` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/mont.png"}`,
+    },
+    {
+      "address": "0x07B838E728787dce14AE1Dec5ACf463bCD9fa16C",
+      "name": "Ober Tech",
+      "symbol": "OBOT",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/obot.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Obor%20Tech` +
+        `&symbol=OBOT` +
+        `&decimals=18` +
+        `&address=0x07B838E728787dce14AE1Dec5ACf463bCD9fa16C` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/obot.png"}`,
+    },
+    {
+      "address": "0xEE4051F59f3c89731a5215738CD999f407956e95",
+      "name": "StorePay",
+      "symbol": "SPC",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/spc.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=StorePay` +
+        `&symbol=SPC` +
+        `&decimals=18` +
+        `&address=0xEE4051F59f3c89731a5215738CD999f407956e95` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/spc.png"}`,
+    },
+    {
+      "address": "0x9Bf115f4493563e3776fd08A07079E641a78cc5b",
+      "name": "Trade",
+      "symbol": "TRD",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/trd.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Trade` +
+        `&symbol=TRD` +
+        `&decimals=18` +
+        `&address=0x9Bf115f4493563e3776fd08A07079E641a78cc5b` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/trd.png"}`,
+    },
+    {
+      "address": "0x1B90fc4c077594D618eE95896012E06aD64B6B54",
+      "name": "Urgo Coin",
+      "symbol": "URG",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/urg.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Urgo%20Coin` +
+        `&symbol=URG` +
+        `&decimals=18` +
+        `&address=0x1B90fc4c077594D618eE95896012E06aD64B6B54` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/urg.png"}`,
+    },
+    {
+      "address": "0xA920211f3554657d975a1837E1721Dc696fa1fa5",
+      "name": "World Plus Coin",
+      "symbol": "WPL",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/wpl.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=World%20Plus%20Coin` +
+        `&symbol=WPL` +
+        `&decimals=18` +
+        `&address=0xA920211f3554657d975a1837E1721Dc696fa1fa5` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/wpl.png"}`,
+    },
+    {
+      "address": "0x0fFb0CAb4AEcfa116A9CF6cE0086455Dc13B95e6",
+      "name": "Zes Coin",
+      "symbol": "ZESC",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/zesc.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=Zes%20Coin` +
+        `&symbol=ZESC` +
+        `&decimals=18` +
+        `&address=0x0fFb0CAb4AEcfa116A9CF6cE0086455Dc13B95e6` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/zesc.png"}`,
     },
   ]
 }
