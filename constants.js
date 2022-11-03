40c4116ac1136f1de6a9d1c10cef8f021dff484b
mnkhod <mnkhod.dev@gmail.com>
2022-11-03 15:10:02 +0800

    Added xARDM to BSC/BSCTest

--- constants.js
+++ constants.js
@@ -664,6 +664,19 @@ const bscTestNetTokenList = {
         `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/ardm.png"}`,
     },
     {
+      "address": "0x2D9ee688D46FD1D39Eb3507BB58dCE3A3cab64D0",
+      "name": "xArdMoney",
+      "symbol": "xARDM",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/xardm.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=xArdMoney` +
+        `&symbol=xARDM` +
+        `&decimals=18` +
+        `&address=0x2D9ee688D46FD1D39Eb3507BB58dCE3A3cab64D0` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/xardm.png"}`,
+    },
+    {
       "address": "0x25A5C86b6CB03D2e5413810Ba5b62cC86473bE70",
       "name": "Inflation Hedging Coin",
       "symbol": "IHC",
@@ -723,6 +736,19 @@ const bscMainNetTokenList = {
         `&decimals=18` +
         `&address=0xE849188F76c0dA93b5eD310a1F72127914b3A7b9` +
         `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/ardm.png"}`,
+    },
+    {
+      "address": "0xE849188F76c0dA93b5eD310a1F72127914b3A7b9",
+      "name": "xArdMoney",
+      "symbol": "xARDM",
+      "decimals": 18,
+      "logoURI": "https://tokenimg.dsolutions.mn/tokens/xardm.png",
+      addToMetamaskLink : `https://metamask.dsolutions.mn/add-token?` +
+        `name=xArdMoney` +
+        `&symbol=xARDM` +
+        `&decimals=18` +
+        `&address=0xE849188F76c0dA93b5eD310a1F72127914b3A7b9` +
+        `&imgUrl=${"https://tokenimg.dsolutions.mn/tokens/xardm.png"}`,
     },
     {
       "address": "0x86a53fcd199212FEa44FA7e16EB1f28812be911D",
